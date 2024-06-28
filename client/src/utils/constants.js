f342f1464b3b86396624520b98cfbf3c8098cb33
Bishwash <bishwash312@gmail.com>
2024-06-28 11:40:38 +0545

    cookie error still fixing

--- client/src/utils/constants.js
+++ client/src/utils/constants.js
@@ -1,4 +1,3 @@
-export const domain = "http://localhost:5555";
-
+// export const domain = "http://localhost:5555";
 // export const domain = "https://lesread.onrender.com";
-// export const domain = "https://les-read.vercel.app";
+export const domain = "https://les-read.vercel.app";
